5b5a8ebc940ad3bfbdcea85fed1c28d062c2ed5b
HADOMIA <b2029hadcomia@pshs.edu.ph>
2025-08-12 08:31:04 +0800

    8th

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ let snack = window.prompt("What snack do you eat every day? : ", "rice crackers"
 let numperday = window.prompt("Now how many do you eat every day? : ", "3");
 let age = window.prompt("Lastly, how old are you now? : ", "14");
 
-if (confirm('Did you enter the correct values for' + age + " " + snack + " " + numperday + " " + maxAge + '?'))
+if (confirm('Did you enter the correct values for ' + age + " " + snack + " " + numperday + " " + maxAge + '?'))
 {
     total = (numperday*365) * (maxAge - age)
     console.log("You will need " + total + " rice crackers until the ripe old age of " + maxAge + ".");
