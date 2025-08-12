77413ef71dd5beb83d391995ed0da08ca299f81c
HADOMIA <b2029hadcomia@pshs.edu.ph>
2025-08-12 08:50:53 +0800

    9th

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 
-let maxAge = window.prompt("Enter your max age : ", "60");
-let snack = window.prompt("What snack do you eat every day? : ", "rice crackers");
-let numperday = window.prompt("Now how many do you eat every day? : ", "3");
-let age = window.prompt("Lastly, how old are you now? : ", "14");
+//let maxAge = window.prompt("Enter your max age : ", "60");
+// let snack = window.prompt("What snack do you eat every day? : ", "rice crackers");
+// let numperday = window.prompt("Now how many do you eat every day? : ", "3");
+// let age = window.prompt("Lastly, how old are you now? : ", "14");
 
 if (confirm('Did you enter the correct values for ' + age + " " + snack + " " + numperday + " " + maxAge + '?'))
 {
