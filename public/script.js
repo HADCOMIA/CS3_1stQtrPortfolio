e7854dba085cbbbbfa0f7b7e7c729f5a59038038
HADOMIA <b2029hadcomia@pshs.edu.ph>
2025-08-12 08:25:46 +0800

    6th commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,15 @@
-var current_age = 14;
-var max_age = 60;
-var numperday = 2;
-var total = (numperday*365)*(max_age - current_age);
 
-document.write("You will need " + total + " rice crackers until the ripe old age of " + max_age + ".");
+let maxAge = window.prompt("Enter your max age : ", "60");
+let snack = window.prompt("What snack do you eat every day? : ", "rice crackers");
+let numperday = window.prompt("Now how many do you eat every day? : ", "3");
+let age = window.prompt("Lastly, how old are you now? : ", "14");
+
+if (confirm('Did you enter the correct values for' + age + snack + numperday + maxAge + '?'))
+{
+    total = (numperday*365) * (maxAge - age)
+    document.log("You will need " + total + " rice crackers until the ripe old age of " + maxAge + ".");
+}
+else
+{
+   window.prompt("Run code again");
+}
